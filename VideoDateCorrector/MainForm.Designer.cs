a3f93786f78397a2488b5cb93e7d3baeaf5de5c3
Spencer Peterson <spencerthomasp@gmail.com>
2018-05-14 13:16:15 -0600

    Nows reads the last 2kb of the file into memory and searches for the 'free' keyword. After it finds 'free' twice it selects the Time/Date string afterward and parses it into a DateTime object. Afterwards it sets the Modified Date of the file to the parsed date. Works with multiple selected files.

---
+++ VideoDateCorrector/MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace VideoDateCorrector
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openMOVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openMOVDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoDisplayTB = new System.Windows.Forms.RichTextBox();
+            this.phoneNameDisplayTB = new System.Windows.Forms.RichTextBox();
+            this.updateFileBtn = new System.Windows.Forms.Button();
+            this.updateDirectoryBtn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(551, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openMOVToolStripMenuItem,
+            this.openMOVDirectoryToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openMOVToolStripMenuItem
+            // 
+            this.openMOVToolStripMenuItem.Name = "openMOVToolStripMenuItem";
+            this.openMOVToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openMOVToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.openMOVToolStripMenuItem.Text = "Open MOV";
+            this.openMOVToolStripMenuItem.Click += new System.EventHandler(this.openMOVToolStripMenuItem_Click);
+            // 
+            // openMOVDirectoryToolStripMenuItem
+            // 
+            this.openMOVDirectoryToolStripMenuItem.Name = "openMOVDirectoryToolStripMenuItem";
+            this.openMOVDirectoryToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.openMOVDirectoryToolStripMenuItem.Text = "Open MOV Directory";
+            this.openMOVDirectoryToolStripMenuItem.Click += new System.EventHandler(this.openMOVDirectoryToolStripMenuItem_Click);
+            // 
+            // infoDisplayTB
+            // 
+            this.infoDisplayTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoDisplayTB.Location = new System.Drawing.Point(13, 28);
+            this.infoDisplayTB.Name = "infoDisplayTB";
+            this.infoDisplayTB.ReadOnly = true;
+            this.infoDisplayTB.Size = new System.Drawing.Size(526, 166);
+            this.infoDisplayTB.TabIndex = 1;
+            this.infoDisplayTB.Text = "";
+            // 
+            // phoneNameDisplayTB
+            // 
+            this.phoneNameDisplayTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneNameDisplayTB.Location = new System.Drawing.Point(13, 219);
+            this.phoneNameDisplayTB.Name = "phoneNameDisplayTB";
+            this.phoneNameDisplayTB.ReadOnly = true;
+            this.phoneNameDisplayTB.Size = new System.Drawing.Size(526, 46);
+            this.phoneNameDisplayTB.TabIndex = 2;
+            this.phoneNameDisplayTB.Text = "";
+            // 
+            // updateFileBtn
+            // 
+            this.updateFileBtn.Location = new System.Drawing.Point(89, 332);
+            this.updateFileBtn.Name = "updateFileBtn";
+            this.updateFileBtn.Size = new System.Drawing.Size(117, 37);
+            this.updateFileBtn.TabIndex = 3;
+            this.updateFileBtn.Text = "Update File Date";
+            this.updateFileBtn.UseVisualStyleBackColor = true;
+            // 
+            // updateDirectoryBtn
+            // 
+            this.updateDirectoryBtn.Location = new System.Drawing.Point(331, 332);
+            this.updateDirectoryBtn.Name = "updateDirectoryBtn";
+            this.updateDirectoryBtn.Size = new System.Drawing.Size(117, 37);
+            this.updateDirectoryBtn.TabIndex = 4;
+            this.updateDirectoryBtn.Text = "Update All In Current Directory";
+            this.updateDirectoryBtn.UseVisualStyleBackColor = true;
+            this.updateDirectoryBtn.Click += new System.EventHandler(this.updateDirectoryBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 407);
+            this.Controls.Add(this.updateDirectoryBtn);
+            this.Controls.Add(this.updateFileBtn);
+            this.Controls.Add(this.phoneNameDisplayTB);
+            this.Controls.Add(this.infoDisplayTB);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "VideoDateCorrector";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openMOVToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openMOVDirectoryToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox infoDisplayTB;
+        private System.Windows.Forms.RichTextBox phoneNameDisplayTB;
+        private System.Windows.Forms.Button updateFileBtn;
+        private System.Windows.Forms.Button updateDirectoryBtn;
+    }
+}
+
---
+++ VideoDateCorrector/MainForm.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace VideoDateCorrector
+{
+    public partial class MainForm : Form
+    {
+        private const int FILECHUNKSIZE = 2048;
+        private string date;
+        private List<string> filePaths = new List<string>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openMOVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "MOV files|*.mov;*.txt";
+                dialog.InitialDirectory = @"C:\";
+                dialog.RestoreDirectory = true;
+                dialog.Title = ".MOV Date Corrector";
+
+                // Allow the user to select multiple files
+                dialog.Multiselect = true;
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string file in dialog.FileNames)
+                    {
+                        try
+                        {
+                            filePaths.Add(file);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error: " + ex.Message);
+                        }
+                    }
+                }
+            }
+                
+        }
+
+        private void openMOVDirectoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string folderName = dialog.SelectedPath;
+
+                    infoDisplayTB.Text = "Directory Opened: " + folderName;
+                }
+            }
+        }
+
+        
+        private void updateDirectoryBtn_Click(object sender, EventArgs e)
+        {
+            
+            byte[] fileChunk = new byte[FILECHUNKSIZE];
+            DateTime fileDate;
+
+            if (filePaths.Count <= 0)
+            {
+                infoDisplayTB.Text = "Please select a file (ctrl+O) or from the dropdown menu first.";
+            }
+            else
+            {
+                infoDisplayTB.Clear();
+
+                foreach (String file in filePaths)
+                {
+
+                    getFileChunk(file, ref fileChunk);
+                    fileDate = getDateFromFileChunk(ref fileChunk);
+
+                    if (fileDate != DateTime.MinValue)
+                    {
+                        File.SetLastWriteTime(file, fileDate);
+                    }
+                    else
+                    {
+                        infoDisplayTB.AppendText("Error parsing file. The date in the file does not match \"yyyy-MM-dd HH:mm:ss\" format");
+                    }
+                }
+                
+            }                        
+        }
+
+        /**
+         * Opens a FileStream and gets the last chunk of a file and reads it into a 
+         * byte array. The size of the chunk is specified by FILECHUNKSIZE in MainForm.cs
+         * @param filePath - The absolute path for the file to be opened in the FileStream.
+         * @param fileChunk - Byte array that is used to hold the binary data from the file. 
+         */ 
+        private void getFileChunk(string filePath, ref byte[] fileChunk)
+        {           
+            using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                // We are only interested in the last [FILECHUNKSIZE] of the file.
+                // This will likely be 2kb unless otherwise specially specified
+                if (stream.Length > FILECHUNKSIZE)
+                {
+                    stream.Seek(-FILECHUNKSIZE, SeekOrigin.End);
+                }
+
+                // Read the last 2kb of the file into memory for faster operations.
+                stream.Read(fileChunk, 0, FILECHUNKSIZE);
+            }
+        }
+
+        // TODO: Error Checking
+        /**
+         * Reads the fileChunk byte array backwards until we find the second "free" string in the file. Once we 
+         * find that we grab the date string that is right in front of it and convert it to a DateTime object.
+         * @param fileChunk - byte array of a file we are trying to iterate over to find two "free" strings.
+         * @return - DateTime object holding the "yyyy-MM-dd HH-mm-ss" format parsed from the fileChunk.
+         */
+        private DateTime getDateFromFileChunk(ref byte[] fileChunk)
+        {
+            DateTime fileDate = new DateTime();
+            int count = 0; ///< This is to keep track how many times we find the word "free"
+            char[] delimiters = { 'T', '-', ':' };
+
+            for (int i = fileChunk.Length - 1; i > 0; i--)
+            {
+                if ((char)fileChunk[i] == 'f')
+                {
+                    if (String.Compare(System.Text.Encoding.UTF8.GetString(fileChunk, i, 4), "free") == 0)
+                    {
+                        count++;
+                    }
+                }
+                else if (count == 2)
+                {
+                    date = System.Text.Encoding.UTF8.GetString(fileChunk, (i - 27), 19);
+
+                    // date has a T in between day and hour in the string, replace with a space.
+                    // Ex. "yyyy-MM-ddTHH:mm:ss" -> "yyyy-MM-dd HH:mm:ss"
+                    date = date.Replace("T", " ");
+
+                    DateTime.TryParseExact(date, "yyyy-MM-dd HH:mm:ss", 
+                        System.Globalization.CultureInfo.InvariantCulture,
+                        System.Globalization.DateTimeStyles.None,
+                        out fileDate);
+
+                    break;
+                }
+            }
+            return fileDate;
+        }
+    }
+}
+
+
+// Read the file backwards, byte by byte using SeekOrigin.Current.
+//long position;
+//stream.Seek(0, SeekOrigin.End);
+//for (position = 0; position < stream.Length; position++)
+//{
+//    stream.Seek(-1, SeekOrigin.Current);
+//    output[0] = (byte)stream.ReadByte();
+//    if (output[0] == 'f')
+//    {
+//        count++;
+//        infoDisplayTB.AppendText(count.ToString() + "\n");
+//    }
+//    stream.Seek(-1, SeekOrigin.Current);
+//}
